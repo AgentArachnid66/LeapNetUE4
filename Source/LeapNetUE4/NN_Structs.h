e4b5868cabe668bcaf7681e3b4dd99fa697d931c
AgentArachnid66 <paulbrown.prb66@gmail.com>
2020-12-12 13:37:35 +0000

    Base Classes

---
+++ Source/LeapNetUE4/NN_Structs.h
@@ -0,0 +1,34 @@
+#pragma once
+#include "NN_Structs.generated.h"
+
+USTRUCT(BlueprintType)
+struct FSynapse {
+	GENERATED_USTRUCT_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		float weight;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		int output;
+};
+
+
+USTRUCT(BlueprintType)
+struct FNeuron {
+
+	GENERATED_USTRUCT_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		TArray<FSynapse> Connections;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		float value;
+};
+
+USTRUCT(BlueprintType)
+struct FNeuralLayer {
+
+	GENERATED_USTRUCT_BODY()
+
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		TArray<FNeuron> NeuronsInLayer;
+};
---
+++ Source/LeapNetUE4/Public/NeuralNetwork.h
@@ -0,0 +1,35 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "NN_Structs.h"
+#include "Components/ActorComponent.h"
+#include "NeuralNetwork.generated.h"
+
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class LEAPNETUE4_API UNeuralNetwork : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UNeuralNetwork();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		TArray<FNeuralLayer> Topology;
+		
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		bool randomiseWeights;
+
+};
